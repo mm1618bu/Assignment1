abdbbc39b81aacda1cb2e357a493d19709375db5
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2021-09-24 13:51:12 -0400

    Update HW1_2.14.py

--- HW1_2.14.py
+++ HW1_2.14.py
@@ -1,12 +1,5 @@
 import math
 x1,y1,x2,y2,x3,y3 = 1.5,-3.4,4.6,5,9.5,-3.4
 print(x1,y1,x2,y2,x3,y3)
-side1 = y1-x1
-side2 = y2-x2
-side3 = y3-x3
-print(side1, side2, side3)
-
-s = (side1+side2+side3)/2
-area = s*(s-side1)*(s-side2)*(s-side3)
+s = ((x1*y2)+(x2*y3)+(x3*y1)-(y1*x2)-(y2*x3)-(y3*x1))/2
 print(s)
-print(area)
