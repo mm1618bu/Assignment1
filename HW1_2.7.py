9b37340b892a5313e2af57fa5c2398b325f07e25
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2021-09-24 21:57:24 -0400

    Update HW1_2.7.py

--- HW1_2.7.py
+++ HW1_2.7.py
@@ -1,7 +1,7 @@
 def calculateTime():
     minutes = int(input("Number of minutes"))
-    years = float(minutes/525600)
-    days = (float(years)-(int(years)))*365
-    print('\n',minutes,'minutes is approximently',int(years),'and',int(days),'days')
+    years = float(minutes/525600) #input number of minutes and divide by number of minutes in a year
+    days = (float(years)-(int(years)))*365 #subtract number of minutes and multiply by 365 days of the year
+    print('\n',minutes,'minutes is approximently',int(years),'and',int(days),'days') #print result
     
 calculateTime()
