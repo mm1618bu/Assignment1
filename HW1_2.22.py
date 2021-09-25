6b28f06e05069d5ad9b9c7c43f555141ddca50c1
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2021-09-24 21:54:31 -0400

    Update HW1_2.22.py

--- HW1_2.22.py
+++ HW1_2.22.py
@@ -6,7 +6,9 @@
 #One birth every 7 seconds (7/60 = 0.1166)
 #one death every 13 seconds (13/60 = 0.2166)
 # One imigrant every 45 seconds (45/60 = 0.75)
-yrs = int(input("Enter the number of years: "))
+
+#This section will determine the inital variables
+yrs = int(input("Enter the number of years: ")) # How many years are in the time period we are evaluating
 population = 312032486
 seconds = 31557600
 birthrate = 7/60
@@ -14,9 +16,9 @@ deathrate = 13/60
 immigratrate = 45/60
 year = 365
 
-born = (yrs*seconds) * birthrate
-die = (yrs*seconds) * deathrate
-imigrate = (yrs*seconds) * immigratrate
+born = (yrs*seconds) * birthrate #Determine the number of people born
+die = (yrs*seconds) * deathrate # Determine the number of people who died
+imigrate = (yrs*seconds) * immigratrate #Determine the number of people who immigrated
 
-final = population + born - die + immigratrate
-print("The population in ",yrs," years is: ",int(final))
+final = population + born - die + immigratrate  #take current population, and births, remove deaths, add immigrants and determine total
+print("The population in ",yrs," years is: ",int(final)) # print the total population and time span
