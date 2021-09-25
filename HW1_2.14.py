7f5931f536c05bcc403814d76936178188aca269
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2021-09-24 21:50:56 -0400

    Update HW1_2.14.py

--- HW1_2.14.py
+++ HW1_2.14.py
@@ -1,5 +1,5 @@
-import math
-x1,y1,x2,y2,x3,y3 = 1.5,-3.4,4.6,5,9.5,-3.4
-print(x1,y1,x2,y2,x3,y3)
-s = ((x1*y2)+(x2*y3)+(x3*y1)-(y1*x2)-(y2*x3)-(y3*x1))/2
-print(s)
+import math #import the math module
+x1,y1,x2,y2,x3,y3 = 1.5,-3.4,4.6,5,9.5,-3.4 #define the value of all of the points of the triangle
+print(x1,y1,x2,y2,x3,y3) #print the values to make sure they are correct
+s = ((x1*y2)+(x2*y3)+(x3*y1)-(y1*x2)-(y2*x3)-(y3*x1))/2 #perform the evaluation of s
+print(s) # print s
