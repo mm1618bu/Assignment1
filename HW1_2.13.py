af06fbe81773e75bf4cdd72e510114de10856d47
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2021-09-24 21:49:46 -0400

    Update HW1_2.13.py

--- HW1_2.13.py
+++ HW1_2.13.py
@@ -1,8 +1,11 @@
+# define a function reverse that takes the parameter of the number to be reversed
 def reverse(numb):
+    #set revers, the initial value, to 0
     revers = 0
+    # if the number put into the parameter is greater then 0, excecute the following
       while numb > 0:
-            remain = numb % 10
-            revers = (revers * 10) + remain
+            remain = numb % 10 #get the remainder for $remain
+            revers = (revers * 10) + remain # add remainder to inital value
             numb = numb // 10
-    print(revers)
+    print(revers) #print the result
 reverse(2345)
