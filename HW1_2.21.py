09d6f3ed0fbcf8628376ed6a0dfe1267c7419e23
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2021-09-24 21:52:22 -0400

    Update HW1_2.21.py

--- HW1_2.21.py
+++ HW1_2.21.py
@@ -1,9 +1,10 @@
-accountbnal = 100
-interest = 1.05
-months = 6
-totalmonth = months-1
-final = (accountbnal*interest)
-for i in range(totalmonth):
+
+accountbnal = 100 # set the initial account balance
+interest = 1.05 # set the interest rate for the account
+months = 6 # set the number of months
+totalmonth = months-1 # remove the end month from being factored in
+final = (accountbnal*interest) # calculate the balance with the interest
+for i in range(totalmonth): # for each month, add 5% interest
     final *= 1.05
-print(int(final))
+print(int(final))# print result
     
