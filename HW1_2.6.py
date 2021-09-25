a7ca977d6f520d4e028343e28f393946e53e67b1
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2021-09-24 21:55:38 -0400

    Update HW1_2.6.py

--- HW1_2.6.py
+++ HW1_2.6.py
@@ -1,5 +1,8 @@
+# Ask user for a number
 enterNumber = input("Enter a number: ")
+# set inital variable to zero
 totalnum = 0
+# for each integer, add 1
 for integer in str(enterNumber):
     totalnum += int(integer)
     
